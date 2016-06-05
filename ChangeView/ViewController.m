4269fb04e6d15850f04b95eac20e4621680e7b2c
Zxxxx <zx62020951@163.com>
2016-06-05 15:50:40 +0800

    注释demo

--- ChangeView/ViewController.m
+++ ChangeView/ViewController.m
@@ -25,37 +25,37 @@
     
     
     
-    /**
-     *  内容添加示例
-     *
-     */
-    //添加子scrollView内容
- [fcVw.headerViews enumerateObjectsUsingBlock:^(UIView * obj, NSUInteger idx, BOOL * _Nonnull stop) {
-     UIImageView *imgVw=[[UIImageView alloc]initWithImage:[UIImage imageNamed:[NSString stringWithFormat:@"%lu",(unsigned long)idx+1]]];
-     imgVw.frame=obj.bounds;
-     
-     NSLog(@"%@",NSStringFromCGRect(obj.bounds));
-     imgVw.contentMode=UIViewContentModeScaleAspectFill;
-     [obj addSubview:imgVw];
-     
- }];
-    
-    //添加底部弹出view内容
-    UIImageView *bottom=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"bottom"]];
-    bottom.frame=fcVw.BottomView.bounds;
-    bottom.contentMode=UIViewContentModeScaleAspectFill;
-    bottom.clipsToBounds=YES;
-    [fcVw.BottomView addSubview:bottom];
-    
-    //添加附加视图内容
-    UIImageView *attach=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"attach"]];
-    attach.frame=fcVw.AttachView.bounds;
-    [fcVw.AttachView addSubview:attach];
-    
-    //添加home视图内容
-    UIImageView *home=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"up"]];
-    home.frame=fcVw.HomeView.bounds;
-    [fcVw.HomeView addSubview:home];
+//    /**
+//     *  内容添加示例
+//     *
+//     */
+//    //添加子scrollView内容
+// [fcVw.headerViews enumerateObjectsUsingBlock:^(UIView * obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//     UIImageView *imgVw=[[UIImageView alloc]initWithImage:[UIImage imageNamed:[NSString stringWithFormat:@"%lu",(unsigned long)idx+1]]];
+//     imgVw.frame=obj.bounds;
+//     
+//     NSLog(@"%@",NSStringFromCGRect(obj.bounds));
+//     imgVw.contentMode=UIViewContentModeScaleAspectFill;
+//     [obj addSubview:imgVw];
+//     
+// }];
+//    
+//    //添加底部弹出view内容
+//    UIImageView *bottom=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"bottom"]];
+//    bottom.frame=fcVw.BottomView.bounds;
+//    bottom.contentMode=UIViewContentModeScaleAspectFill;
+//    bottom.clipsToBounds=YES;
+//    [fcVw.BottomView addSubview:bottom];
+//    
+//    //添加附加视图内容
+//    UIImageView *attach=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"attach"]];
+//    attach.frame=fcVw.AttachView.bounds;
+//    [fcVw.AttachView addSubview:attach];
+//    
+//    //添加home视图内容
+//    UIImageView *home=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"up"]];
+//    home.frame=fcVw.HomeView.bounds;
+//    [fcVw.HomeView addSubview:home];
 
     
 }
